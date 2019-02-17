c84187fb487fedeb74eb330aa7aea61a706ce627
William Budoyo <williambu07@gmail.com>
2019-02-16 20:40:03 -0500

    fizzbuzz and chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    function chunk(array, size) {
+        const chunked = [];
+        let index = 0;
+        
+        while( index < array.length) {
+            chunked.push(array.slice(index, index + size ));
+            index += size;
+        }
+    
+        return chunked;
+    }
+    
+    
+    
+    
+    // function chunk(array, size) {
+    //     chunked = [];
+    //     subArray = [size];
+    
+    //     for( let element of array ) {
+    //         const last = chunked[chunked.length -1];
+    
+    //         if (!last || last.length === size) {
+    //             chunked.push([element]);
+    //         } else {
+    //             last.push(element);
+    //         }
+    //     }
+    
+    //     return chunked;
+    // }
+}
 
 module.exports = chunk;
